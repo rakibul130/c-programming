7b015dbadfe66267e0e7da93235e25e0d1ccf4b0
rakibul130 <72154626+rakibul130@users.noreply.github.com>
2020-10-01 18:50:37 +0600

    Add files via upload

---
+++ rakib2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+         int i,j;
+         for(j=5;i>=1;j--){
+             for(i=0;i<j;i++)
+                 printf("*");
+
+              printf("\n");
+           }
+}
